1b50cb530a4bccdce0b02aefba6ed25bc0e03933
Julio Formiga <julio.formiga@gmail.com>
2025-03-24 03:46:44 +0100

    upd: #5 finalizing player movement

--- src/player.cpp
+++ src/player.cpp
@@ -1,17 +1,29 @@
 #include "player.h"
 
 #include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/System/Angle.hpp>
+#include <SFML/System/Vector2.hpp>
 #include <SFML/Window/Keyboard.hpp>
 #include <cmath>
 
-constexpr float TURN_SPEED = 100.0f;
+constexpr float PI = 3.141592653589793f;
+constexpr float TURN_SPEED = 300.0f;
 constexpr float MOVE_SPEED = 100.0f;
 
 void Player::draw(sf::RenderTarget &target) {
-	sf::CircleShape circle(5.0f);
+	sf::CircleShape circle(8.0f);
+	circle.setOrigin({circle.getRadius(), circle.getRadius()});
 	circle.setPosition(position);
+	circle.setFillColor(sf::Color::Blue);
 
+	sf::RectangleShape line(sf::Vector2f(24.0f, 2.0f));
+	line.setPosition(position);
+	line.setRotation(sf::Angle(sf::degrees(angle)));
+	line.setFillColor(sf::Color::Blue);
+
+	target.draw(line);
 	target.draw(circle);
 }
 
@@ -23,13 +35,13 @@ void Player::update(float deltaTime) {
 		angle += TURN_SPEED * deltaTime;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Up)) {
-		float radians = angle * 3.14159f / 180.0f;
+		float radians = angle * PI / 180.0f;
 
 		position.x += std::cos(radians) * MOVE_SPEED * deltaTime;
 		position.y += std::sin(radians) * MOVE_SPEED * deltaTime;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Down)) {
-		float radians = angle * 3.14159f / 180.0f;
+		float radians = angle * PI / 180.0f;
 
 		position.x -= std::cos(radians) * MOVE_SPEED * deltaTime;
 		position.y -= std::sin(radians) * MOVE_SPEED * deltaTime;
